9986e23bda2dfb2ac45afc56c9c527f07c2418cc
Matteo Zuccon <zuccon.matteo@gmail.com>
2015-07-31 15:13:38 +0200

    test parallel csv

--- src/constant_name.py
+++ src/constant_name.py
@@ -1,6 +1,6 @@
 
-#DATASET_PATH = "/home/pippo/Desktop/PY-UploadToCluster/dataset/smallDataset.csv"
-DATASET_PATH = "/home/pippo/Documents/CIL_EuroImpact_apr_jul_2015_All_ATC_prd_pha_all_10jul2015.csv"
+DATASET_PATH = "/home/pippo/Desktop/PY-UploadToCluster/dataset/smallDataset.csv"
+#DATASET_PATH = "/home/pippo/Documents/CIL_EuroImpact_apr_jul_2015_All_ATC_prd_pha_all_10jul2015.csv"
 ELASTICSEARCH_URL = "http://matteoubuntudev.cloudapp.net:8080"
 INDEX_NAME = "testindice"
 TYPE_NAME = "pharmacy"
