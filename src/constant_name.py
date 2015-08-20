878dd2935591637dcf8ed733607293378845af36
Matteo Zuccon <zuccon.matteo@gmail.com>
2015-08-20 10:26:34 +0200

    v2

--- src/constant_name.py
+++ src/constant_name.py
@@ -1,5 +1,5 @@
-
-### DATASET ####
+﻿
+### DATASET ###
 
 DATASET_PATH = "/home/pippo/py-load-data/dataset/testdataset/CIL_EuroImpact_apr_jul_2015_All_ATC_prd_pha_all_10jul2015.csv"
 #DATASET_PATH = "/home/pippo/Documents/CIL_EuroImpact_apr_jul_2015_All_ATC_prd_pha_all_10jul2015.csv"
@@ -12,16 +12,104 @@ CHUNK_SIZE = 4000
 #ELASTICSEARCH_URL = "http://matteoubuntudev.cloudapp.net:8080"
 ELASTICSEARCH_URL = "http://localhost:9200"
 
-INDEX_NAME = "testindice"
+ELASTICSEARCH_USER ="es_admin"
+ELASTICSEARCH_PASS ="esadmin"
 
+BULK_CHUNK_SIZE = 3000
+
+INDEX_NAME = "agosto"
+NUMBER_SHARDS = 1
+NUMBER_REPLICA = 0
 TYPE_NAME = "pharmacy"
 
-ID_FIELD =""
+TYPE_MAPPING = {
+    TYPE_NAME: {
+        "properties": {
+            "area": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_1_lvl": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_1_lvl_desc": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_2_lvl": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_2_lvl_desc": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_3_lvl": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_3_lvl_desc": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_4_lvl": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_4_lvl_desc": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_5_lvl": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "atc_5_lvl_desc": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "pack_code": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "pack_descr": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "pack_product_manufacturer": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "product_descr": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "product_manufacturer": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "tos": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "tot_qty": {
+                "type": "long"
+            },
+            "week": {
+                "type": "long"
+            },
+            "week_long": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "year_month": {
+                "type": "date",
+                "format":"YYYY-MM"
+            }
+        }
+    }
+}
 
-TIMEOUT_ELASTICSEARCH=300
 
-BULK_CHUNK_SIZE = 2000
 
-NUMBER_SHARDS = 1
-
-NUMBER_REPLICA = 0
--- src/main.py
+++ src/main.py
@@ -1,60 +1,70 @@
-import itertools as IT
+﻿import itertools as IT
 import multiprocessing as mp
 import csv
 import time
 from elasticsearch import Elasticsearch,helpers
+from httplib2 import Http
+from urllib.parse import urlencode
+from random import randint
 import uuid
 import re
 import datetime
-import elasticSearch_wrapper as ELS
 import constant_name as CST
-import data_function as DTF
 
-#es= Elasticsearch("http://matteoubuntudev.cloudapp.net:8080",timeout=120)
-es_instance= ELS.connect_to_elasticsearch_istance(CST.ELASTICSEARCH_URL,CST.TIMEOUT_ELASTICSEARCH)
+es= Elasticsearch(CST.ELASTICSEARCH_URL,timeout=300,http_auth=(CST.ELASTICSEARCH_USER,CST.ELASTICSEARCH_PASS))
 
-def bulk_elasticsearch_data(chunk):
+def bulk_data(chunk):
+   print("into worker",datetime.datetime.now())
    bulk_data=[]
    for line in chunk:
-   	    line["week"] = int(line["week"])
-   	    line["tot_qty"] = int(line["tot_qty"])
+        line["week"] = int(line["week"])
+        line["tot_qty"] = int(line["tot_qty"])
         op_dict = {
 	         "_index": CST.INDEX_NAME, 
 	         "_type": CST.TYPE_NAME, 
-	         "_id": str(uuid.uuid4()),
 	         "_source":line
         }
         bulk_data.append(op_dict)
-   print("(Success, Fail)",datetime.datetime.now())
-   print(helpers.bulk(es_instance,bulk_data,stats_only=True,chunk_size=CST.BULK_CHUNK_SIZE))
-   print("Bulk API Called",datetime.datetime.now())
 
+   print("(Success, Fail)",datetime.datetime.now())
+   bulk_response=helpers.bulk(es,bulk_data,stats_only=False,chunk_size=CST.BULK_CHUNK_SIZE)
+   print(bulk_response[0])
+   print("API Called",datetime.datetime.now())
 
 def main():
-    if(ELS.check_if_index_exists(es_instance,CST.INDEX_NAME)):
-       ELS.delete_index(es_instance,CST.INDEX_NAME)
-    ELS.create_index(es_instance,CST.INDEX_NAME,CST.NUMBER_SHARDS,CST.NUMBER_REPLICA)
+
+
+    create_request_body={
+        "settings": {
+            "number_of_shards":CST.NUMBER_SHARDS,
+            "number_of_replicas":CST.NUMBER_REPLICA
+        }
+    }
+
+    mapping_request_body=CST.TYPE_MAPPING
+
+    index_name= CST.INDEX_NAME
+    if (es.indices.exists(index_name)):
+        es.indices.delete(index=index_name)
+    else:
+        es.indices.create(index=index_name,body=create_request_body)
+        es.indices.put_mapping(doc_type=CST.TYPE_NAME,index=index_name,body=mapping_request_body)
    
     #http://stackoverflow.com/questions/31164731/python-chunking-csv-file-multiproccessing
     start = time.time()
     num_procs = mp.cpu_count()
-    #num_procs=1
-    chunksize = CST.CHUNK_SIZE
+    
     pool = mp.Pool(num_procs)
-    #largefile = "/home/pippo/Documents/CIL_EuroImpact_apr_jul_2015_All_ATC_prd_pha_all_10jul2015.csv"
-    #largefile = "/home/pippo/Documents/Github Repository/Elasticsearch-CSV/dataset/test.csv"
-    #largefile = "/home/pippo/Documents/MOCK_DATA.csv"
-    #largefile = 'Counseling.csv'
-    #largefile = "/home/pippo/Documents/mediumDataset.csv"
-    with open(CST.DATASET_PATH, 'r') as f:
+    largefile = CST.DATASET_PATH
+    with open(largefile, 'r') as f:
         reader = csv.DictReader( f,delimiter=";")
-        for chunk in iter(lambda: list(IT.islice(reader, chunksize*num_procs)), []):
+        for chunk in iter(lambda: list(IT.islice(reader, CST.CHUNK_SIZE*num_procs)), []):
             chunk = iter(chunk)
-            pieces = list(iter(lambda: list(IT.islice(chunk, chunksize)), []))
-            result = pool.map_async(bulk_elasticsearch_data,pieces)
-            print("Bulk function called",datetime.datetime.now())
+            pieces = list(iter(lambda: list(IT.islice(chunk, CST.CHUNK_SIZE)), []))
+            result = pool.map_async(bulk_data,pieces)
+            print("Worker called")
     pool.close()
     pool.join()
-    print("Done",datetime.datetime.now())
-    print("Execution time ",(time.time()-start)/60)
+    print("Done")
+    print((time.time()-start)/60)
 main()
